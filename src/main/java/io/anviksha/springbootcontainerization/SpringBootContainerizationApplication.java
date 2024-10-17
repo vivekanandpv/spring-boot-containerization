d0fe66c0faaaeb1e955920d34d698b9f9cd0f7b4
Vivekanand Pujar <vivek@anviksha.io>
2024-10-17 11:39:47 +0530

    done

---
+++ src/main/java/io/anviksha/springbootcontainerization/SpringBootContainerizationApplication.java
@@ -0,0 +1,13 @@
+package io.anviksha.springbootcontainerization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContainerizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootContainerizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/anviksha/springbootcontainerization/SpringBootContainerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anviksha.springbootcontainerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContainerizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
